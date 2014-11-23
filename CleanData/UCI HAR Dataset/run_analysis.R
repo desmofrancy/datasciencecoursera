c5a20d9c49ddba25d04cdccdfeb35419da8a7555
desmofrancy <francesca.pizzorni@gmail.com>
2014-11-23 15:07:39 +0000

    Commit CleanData module

---
+++ CleanData/UCI HAR Dataset/run_analysis.R
@@ -0,0 +1,85 @@
+#                  GETTING AND CLEANING DATA - COURSE PROJECT                 #
+
+###############################################################################
+#     1. Merges the training and the test sets to create one data set.        #
+###############################################################################
+
+# Loading and merging training and test sets.
+tmp1 <- read.table("train/X_train.txt")
+tmp2 <- read.table("test/X_test.txt")
+X    <- rbind(tmp1, tmp2)
+
+# Loading and merging training and test subjects information. 
+# Each row identifies the subject who performed the activity.
+# Its range is from 1 to 30. 
+tmp1 <- read.table("train/subject_train.txt")
+tmp2 <- read.table("test/subject_test.txt")
+Subjs <- rbind(tmp1, tmp2)
+
+# Loading and merging training and test labels.
+tmp1 <- read.table("train/y_train.txt")
+tmp2 <- read.table("test/y_test.txt")
+y    <- rbind(tmp1, tmp2)
+
+###############################################################################
+#    2. Extracts only the measurements on the mean and standard deviation     #
+###############################################################################
+
+# Loading features
+features <- read.table("features.txt")
+
+# Identify mean() and std() entries (avoiding ambiguity with meanFreq entries)
+# Grep recognises the parentheses by quoting them with \\
+idx_mean_std <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+
+X        <- X[, idx_mean_std]
+names(X) <- features[idx_mean_std, 2]
+names(X) <- gsub("\\(|\\)", "", names(X)) # Removing parentheses from names
+names(X) <- tolower(names(X)) # Converting names to lowercase
+
+###############################################################################
+#  3. Uses descriptive activity names to name the activities in the data set  #
+###############################################################################
+
+# Loading activity labels
+activities      <- read.table("activity_labels.txt")
+
+# Activities name simplification and assignement
+activities[, 2] <- gsub("_", "", tolower(as.character(activities[, 2])))
+y[,1]           <- activities[y[,1], 2]
+names(y)        <- "activity"
+
+###############################################################################
+#    4. Appropriately labels the data set with descriptive variable names.    #
+###############################################################################
+
+# Combination of subjects information, data and features, and table saving.
+names(Subjs) <- "subject"
+clean_table  <- cbind(Subjs, y, X)
+write.table(clean_table, "merged_clean_data.txt")
+
+###############################################################################
+#     5. Creates a 2nd, independent tidy data set with the average of each    #
+#               variable for each activity and each subject.                  #
+###############################################################################
+
+uniqueSubjs   <- unique(Subjs)[,1]
+numSubjs      <- length(unique(Subjs)[,1])
+numActs       <- length(activities[,1])
+numCols       <- dim(clean_table)[2]
+average_table <- clean_table[1:(numSubjs*numActs), ]
+
+row = 1
+for (iSubjs in 1:numSubjs) {
+  for (iActs in 1:numActs) {
+    average_table[row, 1] <- uniqueSubjs[iSubjs]
+    average_table[row, 2] <- activities[iActs, 2]
+    
+    tmp <- clean_table[clean_table$subject==iSubjs & 
+           clean_table$activity==activities[iActs, 2], ]
+    average_table[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+    
+    row = row + 1
+  }
+}
+write.table(average_table, "data_set_with_the_averages.txt")
---
+++ R/Week1_functions.R
@@ -0,0 +1,47 @@
+add2 <- function(x,y) {
+        x + y  
+} 
+
+above10 <- function(x) {
+           use <- x > 10
+           x[use]
+}
+
+aboven <- function(x, n = 10) {
+          use <- x > n
+          x[use]
+}
+
+columnmean <- function(y, removeNA = TRUE) {
+              nc <- ncol(y)
+              means <- numeric(nc)
+              for(i in 1:nc) {
+                       means[i] <- mean(y[, i], na.rm = removeNA)
+              }
+              means
+}
+
+cube <- function(x, n) {
+  x^3
+}
+
+f <- function(x) {
+  g <- function(y) {
+    y + z
+  }
+  z <- 4
+  x + g(x)
+}
+
+h <- function(x, y = NULL, d = 3L) {
+  z <- cbind(x, d)
+  if(!is.null(y))
+    z <- z + y
+  else
+    z <- z + f
+  g <- x + y / z
+  if(d == 3L)
+    return(g)
+  g <- g + 10
+  g
+}
